6747e8f5d1e4a6b614d4fc59bbae3a25df0de197
maybetopy <39399057+maybetopy@users.noreply.github.com>
2018-07-25 01:11:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,1 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on('message', message => {
-if (message.content === "test") {
-  var ms = 20000;
-      var interval = setInterval (function () {
-        message.channel.send("eg")
-      }, ms); 
-    }
-});
-
-client.login('TOKEN');
-client.login(process.env.BOT_TOKEN);
